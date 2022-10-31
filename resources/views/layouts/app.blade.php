d8c469df51a42a9de79fb23196db8f0f4087ca3e
LehaSex <94061794+LehaSex@users.noreply.github.com>
2022-11-01 01:39:16 +0300

    Add elfinder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <?php $dir = "/packages/barryvdh/elfinder/"?>
+        <?php $locale = "en"?>
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
@@ -15,6 +16,38 @@
 
         <!-- Styles -->
         @livewireStyles
+        <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+
+        <!-- elFinder CSS (REQUIRED) -->
+        <link rel="stylesheet" type="text/css" href="<?= asset($dir.'/css/elfinder.min.css') ?>">
+        <link rel="stylesheet" type="text/css" href="<?= asset($dir.'/css/theme.css') ?>">
+
+        <!-- elFinder JS (REQUIRED) -->
+        <script src="<?= asset($dir.'/js/elfinder.min.js') ?>"></script>
+
+        <?php if($locale){ ?>
+        <?php } ?>
+
+        <!-- elFinder initialization (REQUIRED) -->
+        <script type="text/javascript" charset="utf-8">
+            // Documentation for client options:
+            // https://github.com/Studio-42/elFinder/wiki/Client-configuration-options
+            $().ready(function() {
+                $('#elfinder').elfinder({
+                    // set your elFinder options here
+                    <?php if($locale){ ?>
+                        lang: '<?= $locale ?>', // locale
+                    <?php } ?>
+                    customData: { 
+                        _token: '<?= csrf_token() ?>'
+                    },
+                    url : '<?= route("elfinder.connector") ?>',  // connector URL
+                    soundPath: '<?= asset($dir.'/sounds') ?>'
+                });
+            });
+        </script>
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
